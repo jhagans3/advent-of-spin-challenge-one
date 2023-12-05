72c19fe9c8e6e0fc73e9922488d149bb52f241f8
James Hagans <jhagans@acm.org>
2023-12-05 07:20:33 -0500

    update to use uri query
    Signed-off-by: James Hagans <jhagans@acm.org>

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ fn handle_request(req: http::Request<Vec<u8>>) -> anyhow::Result<impl IntoRespon
     let response = match *req.method() {
         Method::POST => {
             // Add the request (URI, body) tuple to the store
-            store.set(req.uri().path(), req.body().as_slice())?;
+            store.set(req.uri().query().unwrap_or(""), req.body().as_slice())?;
             println!(
                 "Storing value in the KV store with {:?} as the key",
-                req.uri().path()
+                req.uri().query().unwrap_or("")
             );
             http::Response::builder()
                 .status(StatusCode::CREATED)
@@ -20,16 +20,22 @@ fn handle_request(req: http::Request<Vec<u8>>) -> anyhow::Result<impl IntoRespon
         }
         Method::GET => {
             // Get the value associated with the request URI, or return a 404 if it's not present
-            match store.get(req.uri().path())? {
+            match store.get(req.uri().query().unwrap_or(""))? {
                 Some(value) => {
-                    println!("Found value for the key {:?}", req.uri().path());
+                    println!(
+                        "Found value for the key {:?}",
+                        req.uri().query().unwrap_or("")
+                    );
                     http::Response::builder()
                         .status(StatusCode::OK)
                         .header("content-type", "application/json")
                         .body(Some(value))?
                 }
                 None => {
-                    println!("No value found for the key {:?}", req.uri().path());
+                    println!(
+                        "No value found for the key {:?}",
+                        req.uri().query().unwrap_or("")
+                    );
                     http::Response::builder()
                         .status(StatusCode::NOT_FOUND)
                         .body(None)?
@@ -38,20 +44,20 @@ fn handle_request(req: http::Request<Vec<u8>>) -> anyhow::Result<impl IntoRespon
         }
         Method::DELETE => {
             // Delete the value associated with the request URI, if present
-            store.delete(req.uri().path())?;
-            println!("Delete key {:?}", req.uri().path());
+            store.delete(req.uri().query().unwrap_or(""))?;
+            println!("Delete key {:?}", req.uri().query().unwrap_or(""));
             http::Response::builder()
                 .status(StatusCode::OK)
                 .body(None)?
         }
         Method::HEAD => {
-            if store.exists(req.uri().path())? {
-                println!("{:?} key found", req.uri().path());
+            if store.exists(req.uri().query().unwrap_or(""))? {
+                println!("{:?} key found", req.uri().query().unwrap_or(""));
                 http::Response::builder()
                     .status(StatusCode::OK)
                     .body(None)?
             } else {
-                println!("{:?} key not found", req.uri().path());
+                println!("{:?} key not found", req.uri().query().unwrap_or(""));
                 http::Response::builder()
                     .status(StatusCode::NOT_FOUND)
                     .body(None)?
